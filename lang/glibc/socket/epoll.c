89b51deb1d156357e4f65b3d35aa0d17327d8827
Ashutosh Singh <aashoo17@gmail.com>
2021-10-21 03:14:40 +0530

    changes

--- lang/glibc/socket/epoll.c
+++ lang/glibc/socket/epoll.c
@@ -5,19 +5,53 @@
 #include <sys/socket.h>
 #include <stdio.h>
 
+/*
+create an empty epoll structure - with nil fd to monitor 
+add fd to this epoll structure 
+events - these are the things we want to track on a fd like read, write readiness
+get the result corresponding to all fd for the corresponding event
+if ready for the event use the normal read/write calls which will not block the thread
+*/
+
 void epoll(){
-    int epfd = epoll_create(100);
+    // create a epoll structure internally pointed by a number epfd
+    // epoll interest list by default is empty - no fd is there to be checked for any kind of events
+    int epfd = epoll_create1(EPOLL_CLOEXEC);
+
+    //TODO: make the fd full fledged server and then monitor here
+    // create a socket fd which we will check for its readiness
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("%d\n",fd);
+    printf("%d\n",fd);  //print to see value
+
+    // create the event we want to track out of a fd
+    /*
+    EPOLLIN - Data other than high-priority data can be read
+    EPOLLPRI - High-priority data can be read
+    EPOLLRDHUP - Shutdown on peer socket (since Linux 2.6.17)
+    EPOLLOUT - Normal data can be written
+    EPOLLET - Employ edge-triggered event notification
+    EPOLLONESHOT - Disable monitoring after event notification
+    EPOLLERR - An error has occurred
+    EPOLLHUP - A hangup has occurred
+    */
     struct epoll_event ev;
     ev.events = EPOLLIN | EPOLLOUT;
-    ev.data.fd = fd;
+    ev.data.fd = fd;    //add the extra data in union as fd
+    // add the fd to be checked for its readiness
+    /*
+    EPOLL_CTL_ADD - add the fd in interest list 
+    EPOLL_CTL_MOD - modify the event we want to check on fd
+    EPOLL_CTL_DEL - delete the fd from interest list
+    */
     epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
 
     struct epoll_event evlist[1];
+    // evlist has array of fds which are ready now - here evlist i.e.  epoll_event's events field will tell for which event we are ready
+    // and its other field comes whatever is added in epoll_ctl will be intact to know for which fd it is 
+    // since we have populated fd field of epoll_event we get back the fd field back
     epoll_wait(epfd, &evlist[0], 1, 0);
 
-    printf("%d\n",evlist[0].data.fd);
+    printf("%d\n",evlist[0].data.fd);   //print the fd field and check if same value of fd in socket creation is matching
 }
 
 int main(){
--- lang/glibc/socket/tcp/server.c
+++ lang/glibc/socket/tcp/server.c
@@ -14,7 +14,7 @@ int main() {
   struct sockaddr_in addr;
   addr.sin_port = htons(3000);
   // todo: conversion functions from human readable 127.0.0.1 to binary big endian form
-  // todo: INADDR_LOOPBACK is const defined for - 127.0.0.1, what is INADDR_ANY then
+  // todo: if INADDR_LOOPBACK is const defined for - 127.0.0.1, what is INADDR_ANY then
   addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
   addr.sin_family = AF_INET;
 
@@ -42,6 +42,7 @@ int main() {
   //todo: is accept not required to put in loop to accept connection from other clients
   // backlog input does point to this theory
   //current server accepts the connection from more than 1 client without keeping accept in  loop
+  // TODO: if in accept() call we keep backlog as 1 then do we have to put accept() in the loop
   int fd = accept(sock, (struct sockaddr *)&cl_addr, &len);
   //this will block the thread and it will accept the connection from other client but will not respond
   //since server will be busy with single client as read/write calls keep on happening in loop
